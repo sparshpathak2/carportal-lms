e80ae7e5ba511d0e96f7696767222d66a707e5c1
Sparsh <sparsh@Sparshs-MacBook-Air.local>
2025-11-25 11:03:25 +0530

    added createLeadStatus route

--- leads-management-service/src/routes/leadStatus.routes.js
+++ leads-management-service/src/routes/leadStatus.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { deleteLeadStatus, getLeadStatusById, getLeadStatuses, updateLeadStatus } from "../controllers/leadStatus.controller.js";
+import { deleteLeadStatus, getLeadStatusById, getLeadStatuses, updateLeadStatus, createLeadStatus } from "../controllers/leadStatus.controller.js";
 
 const router = Router();
 
 // Lead Statuses
 router.get("/", getLeadStatuses)
 router.get("/:id", getLeadStatusById)
+router.post("/", createLeadStatus);
 router.get("/:id", updateLeadStatus)
 router.get("/:id", deleteLeadStatus)
 
