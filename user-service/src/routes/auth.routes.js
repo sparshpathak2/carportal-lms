e5ed602cf9a247169199b11ef1ee7c2384fc8f56
Sparsh <sparshpathak2.0@gmail.com>
2025-12-26 22:03:16 +0530

    fixed import error in user service

--- user-service/src/routes/auth.routes.js
+++ user-service/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, signup, verifySession } from "../controllers/auth.Controller.js";
+import { login, logout, signup, verifySession } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
